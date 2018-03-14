bc12e91342004e948bf656900a29a9fddceec83c
cfalcant <cfalcant@gmail.com>
2018-03-14 12:08:15 -0700

    navbar routes working

--- playdatenow-client/src/components/TopNav.js
+++ playdatenow-client/src/components/TopNav.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { Nav, NavItem, NavLink } from "reactstrap";
+import { Nav, NavItem, NavLink, Navbar} from "reactstrap";
 
 export default class TopNav extends Component {
   render() {
     return (
       <div>
-        <p>Top Nav</p>
+        <p>PlaydateNOW!</p>
+        <Navbar>
         <Nav>
           <NavItem>
-            <NavLink href="#">Link</NavLink>
+            <NavLink href="/">Home</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">Link</NavLink>
+            <NavLink href="/playdates">Playdates</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="#">Another Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink disabled href="#">
-              Disabled Link
+            <NavLink disabled href="">
+              LogOut
             </NavLink>
           </NavItem>
         </Nav>
+        </Navbar>
         <hr />
       </div>
     );
