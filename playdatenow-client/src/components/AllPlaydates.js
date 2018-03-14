d18396dadce3b9b2c1d1a0940b25113ca5a3c0df
cfalcant <cfalcant@gmail.com>
2018-03-14 13:59:09 -0700

    fixed playdates

--- playdatenow-client/src/components/AllPlaydates.js
+++ playdatenow-client/src/components/AllPlaydates.js
@@ -1,41 +1,43 @@
 import React from 'react'
 import { Table } from 'reactstrap'
+import { Link} from 'react-router-dom'
 
 const AllPlaydates = ({playdates}) => {
     // console.log('playdates', playdates)
     
 let mappedPlaydates = playdates.map(playdate => {
-    return <tbody>
-        <tr>
+    return <tr key={playdate.id}>
           <td>{playdate.place}</td>
           <td>{playdate.address}</td>
           <td>{playdate.date}</td>
           <td>{playdate.activity}</td>
           <td>{playdate.notes}</td>
           <td>{playdate.playdate_creator}</td>
+          <td>
+            {/* <Link href="/playdate/:id" /> */}
+          </td>
         </tr>
-      </tbody>;
+      ;
 })
 
-    return (
-        <div>
-            <h5>All Playdates</h5>
+    return <div>
+        <h5>All Playdates</h5>
         <Table hover>
-        <thead>
-          <tr>
-            {/* <th>#</th> */}
-            <th>Place</th>
-            <th>Address</th>
-            <th>Date(s)</th>
-            <th>Activity</th>
-            <th>Notes</th>
-            <th>Playdate Creator</th>
-          </tr>
-        </thead>
-        {mappedPlaydates}
-      </Table>
-      </div>
-    )
+          <thead>
+            <tr>
+              {/* <th>#</th> */}
+              <th>Place</th>
+              <th>Address</th>
+              <th>Date(s)</th>
+              <th>Activity</th>
+              <th>Notes</th>
+              <th>Playdate Creator</th>
+              <th>Join playdate?</th>
+            </tr>
+          </thead>
+          <tbody>{mappedPlaydates}</tbody>
+        </Table>
+      </div>;
 }
 
 export default AllPlaydates
